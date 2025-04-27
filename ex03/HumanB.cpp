0fac02b8c6ea0285ba97ba320581e7a8ff663048
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-04-28 00:16:11 +0100

    ex03 - humanb

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanB.hpp"
 
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
+
+HumanB::~HumanB() {}
+
+void	HumanB::attack() const {
+	if (_weapon)
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	else
+		std::cout << _name << " has no weapons to attack " << std::endl;
+}
+
+void	HumanB::setWeapon(Weapon &weapon) {
+	this->_weapon = &weapon;
+}
