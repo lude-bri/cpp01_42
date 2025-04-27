05fbd1eefdcd458cfb9a1bf5a264ae9034a2f18e
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-04-27 17:03:51 +0100

    ex03 - headers

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,27 +6,25 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:50:40 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/04/27 16:58:29 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/04/27 17:01:07 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEAPON_HPP
 # define WEAPON_HPP
 
-# include <iomanip>
 # include <iostream>
-
 
 class Weapon {
 
 	private:
-		std::string type;
+		std::string _type;
 
 	public:
 		Weapon();
 		~Weapon();
 		
-		std::string	&getType() const;
+		const std::string	&getType() const;
 		void		setType(std::string newValue, std::string type);
 };
 
