cf8f8b449465422eda912031d256b0be99bf7d90
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-04-27 17:08:11 +0100

    ex03 - setting humanA and humanB class

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,18 +6,20 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 17:03:25 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/04/27 17:06:54 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/04/27 17:08:05 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANA_HPP
 # define HUMANA_HPP
 
+#include <iostream>
+
 class HumanA {
 
 	private:
 	public:
-		HumanA();
+		HumanA(std::string name);
 		~HumanA();
 };
 
