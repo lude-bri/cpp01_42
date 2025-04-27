ec99b280562c37242a67ef761da32bedd6185339
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-04-27 16:58:39 +0100

    ex03 - setting weapon class

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,12 +6,29 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:50:40 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/04/27 16:51:15 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/04/27 16:58:29 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEAPON_HPP
 # define WEAPON_HPP
+
+# include <iomanip>
+# include <iostream>
+
+
+class Weapon {
+
+	private:
+		std::string type;
+
+	public:
+		Weapon();
+		~Weapon();
+		
+		std::string	&getType() const;
+		void		setType(std::string newValue, std::string type);
+};
 
 
 #endif
