6a8481760e473eb447ae2eb34979b452541f4f3f
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-01 00:31:06 +0100

    ex05 - OK

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,16 +6,27 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 23:23:31 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/04/30 23:37:49 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/01 00:30:50 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-
-int	main(void)
+int main(void)
 {
-	Harl	harl;
-
-	harl.complain("DEBUG");
+    Harl harl;
+    
+    std::cout << "Testing Harl's complaints:\n" << std::endl;
+   
+	//Teste de validos
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    
+    // Teste com nível inválido
+    std::cout << "Testing invalid level:" << std::endl;
+    harl.complain("INVALID");
+    
+    return 0;
 }
