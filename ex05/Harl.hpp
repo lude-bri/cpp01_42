56e95cde903c8b0afe1a4c00956bfb6c193a8cf8
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-04-30 23:28:02 +0100

    ex05 - starting

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,8 +6,28 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 23:23:30 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/04/30 23:23:43 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/04/30 23:27:49 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+# define HARL_HPP
 
+# include <iostream>
+# include <string>
+
+class Harl {
+
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl();
+		~Harl();
+};
+
+
+#endif
